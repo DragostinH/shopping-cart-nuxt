a1dc19297f1b806c79fbe3a88112ea22b119d1ec
DragostinH <dragostin.hristov@gmail.com>
2022-10-19 22:56:22 +0300

    formatting in Product.vue

--- site/components/menu/Product.vue
+++ site/components/menu/Product.vue
@@ -2,22 +2,15 @@
   <div class="flex flex-col border-r-8 border-primary-red bg-[#221b1a] text-primary-50 p-4 rounded-md">
     <p class="product-title">{{ name }}</p>
     <div class="product-mid-container flex">
-      <img
-          class="product-img"
-          :src="image"
-          title=""
-          style=""
-      />
+      <img class="product-img" :src="image" title="" style="" />
       <p class="product-description">{{ description }}</p>
     </div>
     <div class="product-bottom-container flex">
       <p class="product-price">{{ price }}</p>
       <div class="product-btn-container">
         <div class="add-to-bskt-btn-container">
-          <button class="add-to-basket-btn product-btn">ADD
-          </button
-          >
-          <img class="add-btn-icon" src="https://via.placeholder.com/25x25"/>
+          <button class="add-to-basket-btn product-btn">ADD</button>
+          <img class="add-btn-icon" src="https://via.placeholder.com/25x25" />
         </div>
       </div>
     </div>
@@ -44,8 +37,7 @@ const description = computed(() => props.product.description);
 //use computed property to get the product price
 const price = computed(() => props.product.price);
 //use computed property to get the product image
-const image = computed(() => 'https://via.placeholder.com/280x200');
+const image = computed(() => "https://via.placeholder.com/280x200");
 </script>
 
-<style>
-</style>
+<style></style>
